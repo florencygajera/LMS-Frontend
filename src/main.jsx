05aa4f3b29abafce2c3495ffe0edea0b5ed892b7
florencygajera <florencygajera003@gmail.com>
2026-03-17 17:47:36 +0530

    chore: update package dependencies and enhance main application structure with context providers

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { AuthProvider } from './context/AuthContext.jsx'
+import { AppDataProvider } from './context/AppDataContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <AppDataProvider>
+        <App />
+      </AppDataProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
